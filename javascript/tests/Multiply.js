6bfdc840ffa63218af5f36e3f901a9f2085109a8
Andrew <gandrejko2002@gmail.com>
2023-09-21 10:51:53 +0300

    created repository + added some solutions

---
+++ javascript/tests/Multiply.js
@@ -0,0 +1,11 @@
+import multiply from '../8kyu/Multiply.js';
+import { assert } from 'chai';
+
+describe("Multiply", () => {
+  it("fixed tests", () => {
+    assert.strictEqual(multiply(1,1), 1);
+    assert.strictEqual(multiply(2,1), 2);
+    assert.strictEqual(multiply(2,2), 4);
+    assert.strictEqual(multiply(3,5), 15);
+  });
+});
